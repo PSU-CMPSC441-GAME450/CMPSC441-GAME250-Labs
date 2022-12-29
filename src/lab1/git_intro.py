aa0bb0c373f5022b3f164bc0b6f7141c8ff59456
Pulin Agrawal <pulinagrawal@gmail.com>
2022-12-28 21:27:55 -0500

    posting lab1

---
+++ src/lab1/git_intro.py
@@ -0,0 +1,11 @@
+'''Lab 1.2: Git Intro
+
+In this lab, you will learn how to use git and github to manage your code.
+
+Fork this repository to your own github account.
+Then clone the repository to your local machine.
+Edit this file to: 
+    Print 'Hello <Your Name>' to the console.
+Commit your changes to your local repository.
+Push your changes to your forked repository on github.
+'''
